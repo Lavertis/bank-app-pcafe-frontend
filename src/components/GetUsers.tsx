310441ece6374ff6f9625aff1acb7b821cf66de3
Kelly <pawelkarabowicz@gmail.com>
2022-03-28 23:05:53 +0200

    first commit

--- src/components/GetUsers.tsx
+++ src/components/GetUsers.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import { useState, useEffect } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { useNavigate, useLocation } from "react-router-dom";
+import { AccountInfo } from '../types/accountInfo';
+import { UserInfo } from './UserInfo';
 
 const GetUsers = () => {
-    const [users, setUsers] = useState<[]>();
+    const [account, setAccount] = useState<AccountInfo | null>(null);
     const axiosPrivate = useAxiosPrivate();
     const navigate = useNavigate();
     const location = useLocation();
+
 
     useEffect(() => {
         let isMounted = true;
@@ -15,30 +18,37 @@ const GetUsers = () => {
 
         const getUsers = async () => {
             try {
-                const response = await axiosPrivate.get('/users', {
+                const response= await axiosPrivate.get('https://bank-app-pcafe-api-stage.herokuapp.com/api/Employee', {
                     signal: controller.signal
                 });
-                console.log(response.data);
-                isMounted && setUsers(response.data);
+                isMounted && setAccount(response.data);
+
             } catch (err) {
                 console.error(err);
                 navigate('/login', { state: { from: location }, replace: true });
             }
         }
-
+        
         getUsers();
-
+        
         return () => {
             isMounted = false;
             controller.abort();
         }
     }, [])
+
+    
   return (
-   <div></div>
+        <main>
+            {account && Object.keys(account).map(
+                (c)=>{
+                    let acc = account[c]
+                return <UserInfo account={acc} key={acc.id}/>
+            }
+                )}
+            
+        </main>
   )
 }
 
 export default GetUsers
-
-
-
---
+++ src/types/accountInfo.ts
@@ -0,0 +1,13 @@
+export type AccountInfo = {
+    id: string,
+    firstName: string,
+    lastName: string,
+    salary: number,
+    dateOfEmployment: string
+    appUser: {
+      id:string,
+      userName: string,
+      phoneNumberConfirmed: string
+    }
+
+  };
