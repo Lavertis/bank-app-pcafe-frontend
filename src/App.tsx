807b19ee49b4f22aaf8187185b36ed2fa777fb0a
Kelly <pawelkarabowicz@gmail.com>
2022-03-28 22:40:19 +0200

    adding style

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 /*import logo from "./logo.svg";*/
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import "./App.css";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import Welcome from "./components/Welcome";
 
 function App() {
   return (
-    
-    <div className="App">
+    <div className='min-h-screen bg-slate-100 box-border'>
       <Routes>
+        <Route path='/' element={<Welcome />}/>
         <Route path='/login' element={<Login />} />
         <Route path='/register' element={<Register />} />
       </Routes>
