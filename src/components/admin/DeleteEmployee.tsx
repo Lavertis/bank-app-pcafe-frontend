173a2606fa325fe1d9e37792a3b29cc42278d243
Kelly <pawelkarabowicz@gmail.com>
2022-03-29 22:54:26 +0200

    first commit

---
+++ src/components/admin/DeleteEmployee.tsx
@@ -0,0 +1,61 @@
+import React from 'react'
+import { useState } from "react";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+import { AccountInfo } from '../../types/accountInfo';
+import { UserInfo } from './UserInfo';
+
+
+const Delete_URL = 'api/Employee/delete';
+type FormProps = {
+  onSubmit: (n: string) =>void;
+}
+
+
+const Form = ({onSubmit}: FormProps) =>{
+  const [userId, setUserId] = useState("")
+  const handleSubmit = (event:React.FormEvent<HTMLFormElement>) =>{
+    event.preventDefault();
+    onSubmit(userId)
+  }
+
+  return (
+    <form
+    onSubmit={handleSubmit}
+    >
+    <div>
+      <input placeholder='podaj id'
+      value={userId} 
+      onChange={(e)=>setUserId(e.target.value)}
+      onBlur={(e)=>setUserId(e.target.value)}/>
+      <input type="submit" />
+     </div>
+     </form>
+  )
+}
+
+
+const GetUserById = () =>{
+  const [account,setAccount ] = useState<AccountInfo | null>(null)
+  const axiosPrivate = useAxiosPrivate();
+
+  const handleSubmit = async (n:string) =>{
+    const response = await axiosPrivate.delete(`${Delete_URL}/${n}`)
+    console.log(response)
+    setAccount(response.data)
+    console.log("siema")
+    return (
+        <p>Podane konto zostalo usuniete</p>
+    );
+  }
+
+  return(
+     <main>
+         <Form onSubmit={handleSubmit} />
+     </main>
+
+  )
+
+}
+/*?*
+<UserInfo account={account[0]} />*/
+export default GetUserById
