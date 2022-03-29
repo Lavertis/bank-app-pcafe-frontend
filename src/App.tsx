0a5b3e22b64c0b527e285a78e77197a8850ade83
Kelly <pawelkarabowicz@gmail.com>
2022-03-29 23:00:25 +0200

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,33 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
 import "./App.css";
 import Login from "./components/Login";
-import Register from "./components/Register";
+import CreateEmployeeAccount from "./components/admin/CreateEmployeeAccount";
 import Welcome from "./components/Welcome";
+import WelcomeAdmin from "./components/admin/WelcomeAdmin";
+import RequireRole from "./components/RequireRole";
+import GetUsers from './components/admin/GetUsers';
+import GetUserById from './components/admin/GetUserById';
+import UpdateUser from "./components/admin/UpdateUser";
+import DeleteEmployee from "./components/admin/DeleteEmployee"
 
 function App() {
   return (
     <div className='min-h-screen bg-slate-100 box-border'>
       <Routes>
+        {/*public routes*/}
         <Route path='/' element={<Welcome />}/>
         <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
+
+        {/*protected routes */}
+        
+        {/*<Route element={<RequireRole allowedRole={'Admin'} />}></Route>*/}
+          <Route path='/admin/WelcomeAdmin' element={<WelcomeAdmin />} />
+          <Route path='/admin/GetUsers' element={<GetUsers />} />
+          <Route path='/admin/GetUserByID' element={<GetUserById />} />
+          <Route path='/admin/CreateEmployeeAccount' element={<CreateEmployeeAccount />} />
+          <Route path='/admin/UpdateUser' element={<UpdateUser />} />
+          <Route path='/admin/DeleteEmployee' element={<DeleteEmployee />} />
+        
       </Routes>
     </div>
     
