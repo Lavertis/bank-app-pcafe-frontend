f0f3181603afff981d0a8031abe553f86d4754c5
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-23 12:40:53 +0200

    fix: update api url in axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const env = process.env.NODE_ENV;
 const myAxios = axios.create({
-    baseURL: env === 'development' ? 'https://localhost:7031/api/' : 'https://bank-app-pcafe-api-stage.herokuapp.com/api/',
+    baseURL: env === 'development' ? 'https://localhost:7031/api/' : 'https://bank-app-pcafe-api.herokuapp.com/api/',
     timeout: 5000,
     headers: {
         'Content-Type': 'application/json',
