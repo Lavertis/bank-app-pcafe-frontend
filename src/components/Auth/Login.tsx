12aa09ef0d7559822395bf9b9fc7e79591bca5eb
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-24 17:23:36 +0200

    feat: add floating labels and validation on login screen

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,11 +1,16 @@
 import React, {FC, useContext, useState} from 'react';
-import {Alert, Button, Col, Form} from "react-bootstrap";
+import {Alert, Button, Col, FloatingLabel, Form} from "react-bootstrap";
 import {useFormik} from "formik";
 import {useNavigate} from "react-router-dom";
 import {AxiosError, AxiosResponse} from "axios";
 import {TokenContext} from "../../App";
 import useAxios from "../../hooks/useAxios";
+import * as yup from "yup";
 
+const loginValidationSchema = yup.object().shape({
+    userName: yup.string().required().label('Username'),
+    password: yup.string().required().label('Password')
+});
 
 interface LoginProps {
     redirectTo: string;
@@ -22,6 +27,7 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
             userName: '',
             password: ''
         },
+        validationSchema: loginValidationSchema,
         onSubmit: values => {
             axios.post("auth/authenticate", values)
                 .then((response: AxiosResponse) => {
@@ -43,28 +49,28 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
         <Col xs={11} sm={8} md={6} lg={5} xl={4} xxl={3} className="mx-auto my-auto bg-light rounded-3 p-5 shadow">
             {error && <Alert variant="danger" className="text-center">{error}</Alert>}
             <Form onSubmit={formik.handleSubmit}>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputUsername">Username</Form.Label>
+                <FloatingLabel controlId="inputUserName" label="Username" className="mb-3">
                     <Form.Control
                         type="text"
                         name="userName"
-                        id="inputUsername"
+                        placeholder="Username"
                         onChange={formik.handleChange}
                         value={formik.values.userName}
-                        required
+                        isInvalid={formik.touched.userName && !!formik.errors.userName}
                     />
-                </Form.Group>
-                <Form.Group className="mb-3">
-                    <Form.Label htmlFor="inputPassword">Password</Form.Label>
+                    <Form.Control.Feedback type="invalid">{formik.errors.userName}</Form.Control.Feedback>
+                </FloatingLabel>
+                <FloatingLabel controlId="inputPassword" label="Password" className="mb-3">
                     <Form.Control
                         type="password"
                         name="password"
-                        id="inputPassword"
+                        placeholder="Password"
                         onChange={formik.handleChange}
                         value={formik.values.password}
-                        required
+                        isInvalid={formik.touched.password && !!formik.errors.password}
                     />
-                </Form.Group>
+                    <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+                </FloatingLabel>
                 <Form.Group className="d-grid">
                     <Button type="submit" variant="primary">Login</Button>
                 </Form.Group>
