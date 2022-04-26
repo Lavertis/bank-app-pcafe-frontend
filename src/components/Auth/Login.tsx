d228e6ad1226985020ae24882317596564896677
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-04-26 09:21:11 +0200

    fix: validation errors in login component

--- src/components/Auth/Login.tsx
+++ src/components/Auth/Login.tsx
@@ -1,14 +1,15 @@
-import React, {FC, useContext, useState} from 'react';
+import React, {FC, useContext} from 'react';
 import {Alert, Button, Col, FloatingLabel, Form} from "react-bootstrap";
 import {useFormik} from "formik";
 import {useNavigate} from "react-router-dom";
 import {TokenContext} from "../../App";
 import useAxios from "../../hooks/useAxios";
 import * as yup from "yup";
+import {getErrorsWithFirstMessages} from "../../helpers/fluent-validation";
 
 const loginValidationSchema = yup.object().shape({
-    userName: yup.string().required().label('Username'),
-    password: yup.string().required().label('Password')
+    UserName: yup.string().required().label('Username'),
+    Password: yup.string().required().label('Password')
 });
 
 interface LoginProps {
@@ -19,12 +20,12 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
     const {setToken} = useContext(TokenContext)
     const axios = useAxios()
     const navigate = useNavigate()
-    const [generalError, setGeneralError] = useState("")
+    const [serverError, setServerError] = React.useState<string>('')
 
     const formik = useFormik({
         initialValues: {
-            userName: '',
-            password: ''
+            UserName: '',
+            Password: ''
         },
         validationSchema: loginValidationSchema,
         onSubmit: values => {
@@ -37,7 +38,9 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
                 })
                 .catch(error => {
                     if (error.response && error.response.status >= 400 && error.response.status <= 500) {
-                        setGeneralError(error.response.data.error['name'] + ": " + error.response.data.error['description'])
+                        const errors = getErrorsWithFirstMessages(error.response.data.errors)
+                        formik.setErrors(errors)
+                        setServerError(error.response.data.error)
                     }
                 })
         },
@@ -46,29 +49,29 @@ const Login: FC<LoginProps> = ({redirectTo}) => {
     return (
         <Col xs={11} sm={8} md={6} lg={5} xl={4} xxl={3} className="mx-auto my-auto bg-light rounded-3 p-5 shadow">
             <h3 className="mb-4">Sign In</h3>
-            {generalError && <Alert variant="danger" className="text-center">{generalError}</Alert>}
+            {serverError && <Alert variant="danger" className="text-center">{serverError}</Alert>}
             <Form onSubmit={formik.handleSubmit} noValidate>
                 <FloatingLabel controlId="inputUserName" label="Username" className="mb-3">
                     <Form.Control
                         type="text"
-                        name="userName"
+                        name="UserName"
                         placeholder="Username"
                         onChange={formik.handleChange}
-                        value={formik.values.userName}
-                        isInvalid={formik.touched.userName && !!formik.errors.userName}
+                        value={formik.values.UserName}
+                        isInvalid={formik.touched.UserName && !!formik.errors.UserName}
                     />
-                    <Form.Control.Feedback type="invalid">{formik.errors.userName}</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">{formik.errors.UserName}</Form.Control.Feedback>
                 </FloatingLabel>
                 <FloatingLabel controlId="inputPassword" label="Password" className="mb-3">
                     <Form.Control
                         type="password"
-                        name="password"
+                        name="Password"
                         placeholder="Password"
                         onChange={formik.handleChange}
-                        value={formik.values.password}
-                        isInvalid={formik.touched.password && !!formik.errors.password}
+                        value={formik.values.Password}
+                        isInvalid={formik.touched.Password && !!formik.errors.Password}
                     />
-                    <Form.Control.Feedback type="invalid">{formik.errors.password}</Form.Control.Feedback>
+                    <Form.Control.Feedback type="invalid">{formik.errors.Password}</Form.Control.Feedback>
                 </FloatingLabel>
                 <Form.Group className="d-grid mt-4">
                     <Button type="submit" variant="primary">Sign In</Button>
