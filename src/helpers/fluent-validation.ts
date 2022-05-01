998c5590b255f1a6fa0ae158955acec9875d5936
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-01 06:01:46 +0200

    fix: check if response in axios catch has api validation errors (#10)

--- src/helpers/fluent-validation.ts
+++ src/helpers/fluent-validation.ts
@@ -1,5 +1,7 @@
 export function getErrorsWithFirstMessages(errors: {}) {
     const result = {};
+    if (!errors) return result;
+
     Object.keys(errors).forEach(key => {
         if (Array.isArray(errors[key as keyof typeof errors])) {
             result[key as keyof typeof errors] = errors[key as keyof typeof errors][0];
