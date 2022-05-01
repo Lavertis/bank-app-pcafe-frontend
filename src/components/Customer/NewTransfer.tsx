02eb70e448a00ba56e2c101e3446d5cea0303976
Rafał Kuźmiczuk <29688908+Lavertis@users.noreply.github.com>
2022-05-01 20:23:19 +0200

    fix: don't display new transfer form if customer has no active accounts

--- src/components/Customer/NewTransfer.tsx
+++ src/components/Customer/NewTransfer.tsx
@@ -26,7 +26,8 @@ const NewTransfer: FC<NewTransferProps> = () => {
     const axios = useAxios()
     const navigate = useNavigate()
     const [accounts, setAccounts] = useState<Account[]>([]);
-    const [serverError, setServerError] = React.useState<string>('')
+    const [isDataFetched, setIsDataFetched] = useState(false);
+    const [serverError, setServerError] = useState<string>('')
 
     const formik = useFormik({
         initialValues: {
@@ -60,10 +61,13 @@ const NewTransfer: FC<NewTransferProps> = () => {
     useEffect(() => {
         axios.get(`account-management/customers/auth/accounts`)
             .then(response => {
-                setAccounts(response.data)
-                if (response.data.length > 0) {
-                    formik.setFieldValue('SenderAccountId', response.data[0].id)
+                const accounts = response.data as Account[]
+                const activeAccounts = accounts.filter(account => account.isActive)
+                setAccounts(activeAccounts)
+                if (activeAccounts.length > 0) {
+                    formik.setFieldValue('SenderAccountId', activeAccounts[0].id.toString())
                 }
+                setIsDataFetched(true)
             })
             .catch(error => {
                 console.log(error)
@@ -72,85 +76,96 @@ const NewTransfer: FC<NewTransferProps> = () => {
     }, [axios])
 
     return (
-        <Col xs={11} sm={8} md={6} lg={5} xl={4} className="mx-auto my-5 bg-light rounded-3 p-5 shadow">
-            <h3 className="mb-4">Send transfer</h3>
-            {serverError && <Alert variant="danger" className="text-center">{serverError}</Alert>}
-            <Form onSubmit={formik.handleSubmit} noValidate>
-                <Form.Floating className="mb-3 flex-grow-1 input-group">
-                    <Form.Control
-                        type="number"
-                        name="Amount"
-                        id="inputAmount"
-                        placeholder="Amount"
-                        min={0}
-                        step={1}
-                        onChange={formik.handleChange}
-                        value={formik.values.Amount}
-                        isValid={formik.touched.Amount && !formik.errors.Amount}
-                        isInvalid={formik.touched.Amount && !!formik.errors.Amount}
-                    />
-                    <InputGroup.Text className="rounded-end">{getSelectedAccountCurrency()}</InputGroup.Text>
-                    <label htmlFor="inputSalary" className="z-index-3">Amount</label>
-                    <Form.Control.Feedback type="invalid">{formik.errors.Amount}</Form.Control.Feedback>
-                </Form.Floating>
-                <FloatingLabel controlId="inputSenderAccountId" label="From account" className="mb-3">
-                    <Form.Select
-                        name="SenderAccountId"
-                        id="inputSenderAccountId"
-                        onChange={formik.handleChange}
-                        value={formik.values.SenderAccountId}
-                        // isValid={formik.touched.SenderAccountId && !formik.errors.SenderAccountId}
-                        isInvalid={formik.touched.SenderAccountId && !!formik.errors.SenderAccountId}>
-                        {accounts.map(account => (
-                            account.isActive && <option key={account.id} value={account.id}>
-                                {account.number} ({account.balance.toFixed(2)} {account.currencyCode})
-                            </option>
-                        ))}
-                    </Form.Select>
-                    <Form.Control.Feedback type="invalid">{formik.errors.SenderAccountId}</Form.Control.Feedback>
-                </FloatingLabel>
-                <FloatingLabel controlId="inputReceiverAccountNumber" label="Receiver's account number"
-                               className="mb-3">
-                    <Form.Control
-                        type="text"
-                        name="ReceiverAccountNumber"
-                        placeholder="Receiver's account number"
-                        onChange={formik.handleChange}
-                        value={formik.values.ReceiverAccountNumber}
-                        // isValid={formik.touched.ReceiverAccountNumber && !formik.errors.ReceiverAccountNumber}
-                        isInvalid={formik.touched.ReceiverAccountNumber && !!formik.errors.ReceiverAccountNumber}
-                    />
-                    <Form.Control.Feedback type="invalid">{formik.errors.ReceiverAccountNumber}</Form.Control.Feedback>
-                </FloatingLabel>
-                <FloatingLabel controlId="inputReceiverName" label="Receiver's name" className="mb-3">
-                    <Form.Control
-                        type="text"
-                        name="ReceiverName"
-                        placeholder="Receiver's name"
-                        onChange={formik.handleChange}
-                        value={formik.values.ReceiverName}
-                        isValid={formik.touched.ReceiverName && !formik.errors.ReceiverName}
-                        isInvalid={formik.touched.ReceiverName && !!formik.errors.ReceiverName}
-                    />
-                    <Form.Control.Feedback type="invalid">{formik.errors.ReceiverName}</Form.Control.Feedback>
-                </FloatingLabel>
-                <FloatingLabel controlId="inputTitle" label="Title" className="mb-3">
-                    <Form.Control
-                        type="text"
-                        name="Title"
-                        placeholder="Title"
-                        onChange={formik.handleChange}
-                        value={formik.values.Title}
-                        isValid={formik.touched.Title && !formik.errors.Title}
-                        isInvalid={formik.touched.Title && !!formik.errors.Title}
-                    />
-                    <Form.Control.Feedback type="invalid">{formik.errors.Title}</Form.Control.Feedback>
-                </FloatingLabel>
-                <Form.Group className="d-grid mt-4">
-                    <Button type="submit" variant="primary">Send transfer</Button>
-                </Form.Group>
-            </Form>
-        </Col>
+        <>
+            {isDataFetched && (!accounts.length ?
+                    <Alert variant="primary" className="mt-5 mx-auto">
+                        You don't have any (active) account to make a transfer
+                    </Alert> :
+                    <Col xs={11} sm={8} md={6} lg={5} xl={4} className="mx-auto my-5 bg-light rounded-3 p-5 shadow">
+                        <h3 className="mb-4">Send transfer</h3>
+                        {serverError && <Alert variant="danger" className="text-center">{serverError}</Alert>}
+                        <Form onSubmit={formik.handleSubmit} noValidate>
+                            <Form.Floating className="mb-3 flex-grow-1 input-group">
+                                <Form.Control
+                                    type="number"
+                                    name="Amount"
+                                    id="inputAmount"
+                                    placeholder="Amount"
+                                    min={0}
+                                    step={1}
+                                    onChange={formik.handleChange}
+                                    value={formik.values.Amount}
+                                    isValid={formik.touched.Amount && !formik.errors.Amount}
+                                    isInvalid={formik.touched.Amount && !!formik.errors.Amount}
+                                />
+                                <InputGroup.Text
+                                    className="rounded-end">{getSelectedAccountCurrency()}</InputGroup.Text>
+                                <label htmlFor="inputSalary" className="z-index-3">Amount</label>
+                                <Form.Control.Feedback type="invalid">{formik.errors.Amount}</Form.Control.Feedback>
+                            </Form.Floating>
+                            <FloatingLabel controlId="inputSenderAccountId" label="From account" className="mb-3">
+                                <Form.Select
+                                    name="SenderAccountId"
+                                    id="inputSenderAccountId"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.SenderAccountId}
+                                    // isValid={formik.touched.SenderAccountId && !formik.errors.SenderAccountId}
+                                    isInvalid={formik.touched.SenderAccountId && !!formik.errors.SenderAccountId}>
+                                    {accounts.map(account => (
+                                        account.isActive && <option key={account.id} value={account.id}>
+                                            {account.number} ({account.balance.toFixed(2)} {account.currencyCode})
+                                        </option>
+                                    ))}
+                                </Form.Select>
+                                <Form.Control.Feedback
+                                    type="invalid">{formik.errors.SenderAccountId}</Form.Control.Feedback>
+                            </FloatingLabel>
+                            <FloatingLabel controlId="inputReceiverAccountNumber" label="Receiver's account number"
+                                           className="mb-3">
+                                <Form.Control
+                                    type="text"
+                                    name="ReceiverAccountNumber"
+                                    placeholder="Receiver's account number"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.ReceiverAccountNumber}
+                                    // isValid={formik.touched.ReceiverAccountNumber && !formik.errors.ReceiverAccountNumber}
+                                    isInvalid={formik.touched.ReceiverAccountNumber && !!formik.errors.ReceiverAccountNumber}
+                                />
+                                <Form.Control.Feedback
+                                    type="invalid">{formik.errors.ReceiverAccountNumber}</Form.Control.Feedback>
+                            </FloatingLabel>
+                            <FloatingLabel controlId="inputReceiverName" label="Receiver's name" className="mb-3">
+                                <Form.Control
+                                    type="text"
+                                    name="ReceiverName"
+                                    placeholder="Receiver's name"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.ReceiverName}
+                                    isValid={formik.touched.ReceiverName && !formik.errors.ReceiverName}
+                                    isInvalid={formik.touched.ReceiverName && !!formik.errors.ReceiverName}
+                                />
+                                <Form.Control.Feedback
+                                    type="invalid">{formik.errors.ReceiverName}</Form.Control.Feedback>
+                            </FloatingLabel>
+                            <FloatingLabel controlId="inputTitle" label="Title" className="mb-3">
+                                <Form.Control
+                                    type="text"
+                                    name="Title"
+                                    placeholder="Title"
+                                    onChange={formik.handleChange}
+                                    value={formik.values.Title}
+                                    isValid={formik.touched.Title && !formik.errors.Title}
+                                    isInvalid={formik.touched.Title && !!formik.errors.Title}
+                                />
+                                <Form.Control.Feedback type="invalid">{formik.errors.Title}</Form.Control.Feedback>
+                            </FloatingLabel>
+                            <Form.Group className="d-grid mt-4">
+                                <Button type="submit" variant="primary">Send transfer</Button>
+                            </Form.Group>
+                        </Form>
+                    </Col>
+            )}
+        </>
     );
 }
 
